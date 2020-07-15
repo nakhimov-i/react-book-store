608f71b97a349e4831442377c74ba35628e6b22c
nakhimov-i <62019196+nakhimov-i@users.noreply.github.com>
2020-07-15 15:02:21 +0300

    fix bug

--- src/components/Product.js
+++ src/components/Product.js
@@ -5,7 +5,7 @@ import {ProductConsumer} from '../context'
 
 export default class Product extends Component {
     render() {
-        const {id, title, pic, price, inCart} = this.props.product
+        const {id, title, img, price, inCart} = this.props.product
         return (
             <ProductWrapper className='col-9 col-md-6 col-lg-3 mx-auto my-3'>
                <div className='card'>
@@ -13,7 +13,7 @@ export default class Product extends Component {
                        {value => (
                             <div className='img-container p-5' onClick={() => value.handleDetail(id)}>
                                 <Link to='/details'>
-                                    <img src={pic} alt='product image' className='card-img-top'/>
+                                    <img src={img} alt='product image' className='card-img-top'/>
                                 </Link>
                                 <button className='cart-btn' disabled={inCart ? true : false} onClick={() => {value.addToCart(id); value.openModal(id)}}>
                                     {inCart ? (<p className='text-capitalize mb-0' disabled>in Cart</p>) : (<i className='fas fa-cart-plus'/>)}
